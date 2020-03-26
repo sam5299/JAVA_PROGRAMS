5a2675a0fe18d71b8018b08c48c0ff5bb370a8c9
Sajitha-Vijayan <59993341+Sajitha-Vijayan@users.noreply.github.com>
2020-03-26 18:20:03 +0530

    Update Arithmetic.java

--- src/Java_Lab6/Arithmetic.java
+++ src/Java_Lab6/Arithmetic.java
@@ -1,4 +1,5 @@
-
+/* Program to create interface named test. In this interface the member function is square. Implement this interface in arithmetic class.
+Create one new class called ToTestInt in this class use the object of arithmetic class. */
 package Java_Lab6;
 import java.util.*;
 interface test
