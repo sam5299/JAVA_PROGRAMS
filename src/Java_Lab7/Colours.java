d9382d8fdd1ace9db759d7e0de0c915cc6cbc311
Sajitha-Vijayan <59993341+Sajitha-Vijayan@users.noreply.github.com>
2020-03-26 18:11:26 +0530

    Update Colours.java

--- src/Java_Lab7/Colours.java
+++ src/Java_Lab7/Colours.java
@@ -1,8 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*Write a class called ColourChecking. Define a color with red = 193, green=255 
+and blue = 183. No separate the rgb values. Find the Hue, saturation and brightness of this color.*/
 package Java_Lab7;
 import java.awt. *;
 
