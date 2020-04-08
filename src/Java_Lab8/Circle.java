193500b8f32e8610fab5389ab65aeba23e5c10d1
Sajitha-Vijayan <59993341+Sajitha-Vijayan@users.noreply.github.com>
2020-04-08 19:09:17 +0530

    Java_Lab8.3 Program

---
+++ src/Java_Lab8/Circle.java
@@ -0,0 +1,16 @@
+/*8.3
+A program to create a package named mypack and import it in circle class. 
+*/
+package Java_Lab8;
+
+import mypack.Example;
+import java.util.*;
+
+public class Circle 
+{
+    public static void main(String args[])
+    {
+        Example e=new Example();
+        e.area();
+    }
+}
