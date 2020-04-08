eba3f40363cedd9a47704eb5ba0c48a53cc313c3
Sajitha-Vijayan <59993341+Sajitha-Vijayan@users.noreply.github.com>
2020-04-08 19:09:35 +0530

    Java_Lab8.4 Program

---
+++ src/Java_Lab8/Ex1.java
@@ -0,0 +1,16 @@
+/*8.4
+A program to create a package named  pl, and implement this package in ex1 class. 
+*/
+package Java_Lab8;
+
+import p1.P1_Example;
+import java.util.*;
+
+public class Ex1 
+{
+    public static void main(String args[])
+    {
+        P1_Example e=new P1_Example();
+        e.sum();
+    }
+}
